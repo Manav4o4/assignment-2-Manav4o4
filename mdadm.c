c10757391e922e0a25eca8a99e7250cf7c1fec5f
Manav Bajaj <manavbajaj253@gmail.com>
2024-10-13 21:58:52 -0400

    First Draft

--- mdadm.c
+++ mdadm.c
@@ -37,7 +37,22 @@ int mdadm_unmount(void) {
 }
 
 int mdadm_read(uint32_t start_addr, uint32_t read_len, uint8_t *read_buf)  {
-  //Complete your code here
-  return 0;
+
+	uint32_t total_storage = JBOD_NUM_DISKS * JBOD_NUM_BLOCKS_PER_DISK * JBOD_BLOCK_SIZE;
+
+	if (start_addr + read_len > total_storage){
+		return -1;
+	}
+
+	if (read_len > 1024){
+		return -2;
+	}
+
+	if (is_mounted == 0){
+		return -3;
+	}
+
+	return -4;
+
 }
 
