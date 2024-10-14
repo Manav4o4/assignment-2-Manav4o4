b2b6b2ca918e803570b2b298be504983659323ef
Manav Bajaj <manavbajaj253@gmail.com>
2024-10-13 21:04:23 -0400

    First Commit

--- mdadm.c
+++ mdadm.c
@@ -5,13 +5,21 @@
 #include "jbod.h"
 
 int mdadm_mount(void) {
-  // Complete your code here
-  return 0;
+  if(jbod_operation(JBOD_MOUNT, NULL) == 0){
+	  return 1;
+  }
+  else{
+	  return -1;
+  }
 }
 
 int mdadm_unmount(void) {
-  //Complete your code here
-  return 0;
+  if(jbod_operation(JBOD_UNMOUNT, NULL) == 0){
+	  return 1;
+  }
+  else{
+	  return -1;
+  }
 }
 
 int mdadm_read(uint32_t start_addr, uint32_t read_len, uint8_t *read_buf)  {
