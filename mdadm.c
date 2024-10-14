a6d64b96f49125af42c938fd8ceb5d9e458e7dc9
Manav Bajaj <manavbajaj253@gmail.com>
2024-10-13 21:36:31 -0400

    Fixed mount and unmount

--- mdadm.c
+++ mdadm.c
@@ -4,22 +4,36 @@
 #include "mdadm.h"
 #include "jbod.h"
 
+static int is_mounted = 0;
+
 int mdadm_mount(void) {
-  if(jbod_operation(JBOD_MOUNT, NULL) == 0){
-	  return 1;
-  }
-  else{
-	  return -1;
-  }
+
+	if (is_mounted == 1){
+		return -1;
+	}
+
+	uint32_t op = (0 << 12);
+
+	if(jbod_operation(op, NULL) == 0){
+		is_mounted = 1;
+		return 1;
+	}
+	else{
+		return -1;
+	}
 }
 
 int mdadm_unmount(void) {
-  if(jbod_operation(JBOD_UNMOUNT, NULL) == 0){
-	  return 1;
-  }
-  else{
-	  return -1;
-  }
+
+	uint32_t op = (1 << 12);
+
+	if(jbod_operation(op, NULL) == 0){
+		return 1;
+	}
+	else{
+		return -1;
+	}
+	
 }
 
 int mdadm_read(uint32_t start_addr, uint32_t read_len, uint8_t *read_buf)  {
